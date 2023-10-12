db3062b218ee37296cf2dc1f504384994f645b3e
rayanramoul <raysamram@gmail.com>
2023-10-12 18:22:09 +0200

    :sparkles: add feature maps visualization base

--- src/model.py
+++ src/model.py
@@ -1,8 +1,10 @@
+import imp
 import torch
 import torch.nn as nn
 import torchvision
 import os
 from torch.backends import cudnn
+import matplotlib.pyplot as plt
 
 cudnn.benchmark = True
 
@@ -19,7 +21,30 @@ class ResNet():
         self.model.to(self.device)
         self.model = self.model.to(self.device)
         self.inputs_ckpt = os.path.join("checkpoints", "resnet.pth")
-        print(f"\nMODEL SUMMARY:\n\n{self.model}\n")
+        
+        
+        # we will save the conv layer weights in this list
+        self.model_weights =[]
+        #we will save the 49 conv layers in this list
+        self.conv_layers = []# get all the model children as list
+        self.model_children = list(self.model.children())#counter to keep count of the conv layers
+        counter = 0#append all the conv layers and their respective wights to the list
+        for i in range(len(self.model_children)):
+            if type(self.model_children[i]) == nn.Conv2d:
+                counter+=1
+                self.model_weights.append(self.model_children[i].weight)
+                self.conv_layers.append(self.model_children[i])
+            elif type(self.model_children[i]) == nn.Sequential:
+                for j in range(len(self.model_children[i])):
+                    for child in self.model_children[i][j].children():
+                        if type(child) == nn.Conv2d:
+                            counter+=1
+                            self.model_weights.append(child.weight)
+                            self.conv_layers.append(child)
+        print(f"Total convolution layers: {counter}")
+        print("conv_layers")
+        
+        # print(f"\nMODEL SUMMARY:\n\n{self.model}\n")
 
     def load_checkpoint(self, inputs_ckpt=None):
         if inputs_ckpt:
@@ -31,4 +56,29 @@ class ResNet():
         if inputs_ckpt:
             self.inputs_ckpt = inputs_ckpt
         torch.save(self.model.state_dict(), self.inputs_ckpt)
+        
+    def visualize_feature_maps(self, image):
+        outputs = []
+        names = []
+        for layer in self.conv_layers[0:]:
+            image = layer(image)
+            outputs.append(image)
+            names.append(str(layer))
+        print(len(outputs))#print feature_maps
+        for feature_map in outputs:
+            print(feature_map.shape)
+        processed = []
+        for feature_map in outputs:
+            feature_map = feature_map.squeeze(0)
+            gray_scale = torch.sum(feature_map,0)
+            gray_scale = gray_scale / feature_map.shape[0]
+            processed.append(gray_scale.data.cpu().numpy())
+            
+        fig = plt.figure(figsize=(30, 50))
+        for i in range(len(processed)):
+            a = fig.add_subplot(5, 4, i+1)
+            imgplot = plt.imshow(processed[i])
+            a.axis("off")
+            a.set_title(names[i].split('(')[0], fontsize=30)
+        plt.savefig(str('feature_maps.jpg'), bbox_inches='tight')
         
