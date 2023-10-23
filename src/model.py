a54fdd40f457db4dca274b1e3e2de8f34555e017
rayanramoul <raysamram@gmail.com>
2023-10-23 13:54:59 +0200

    :sparkles: new train script and custom resnet to select stop layers for training

--- src/model.py
+++ src/model.py
@@ -8,19 +8,116 @@ import matplotlib.pyplot as plt
 
 cudnn.benchmark = True
 
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+        
+    def forward(self, x):
+        return x
+
+
+# Create a custom ResNet model with the desired changes
+resnet_model = torchvision.models.resnet50(pretrained=True)
+# Create a custom ResNet model with the desired changes
+class CustomResNet(nn.Module):
+    def __init__(self, replace_from_layer, num_classes=2, device="gpu"):
+        super(CustomResNet, self).__init__()
+        self.device = device
+        self.features = nn.Sequential(*list(resnet_model.children())[:replace_from_layer]).to(self.device)
+        self.identity = Identity().to(self.device)
+        # Calculate the number of input features for the final classifier
+        self.num_features = self._calculate_num_features(replace_from_layer)
+        self.classifier = nn.Sequential(
+            nn.Linear(self.num_features, num_classes),
+        ).to(self.device)
+        print("\n\nCustom ResNet Model :")
+        print("self.features : ", self.features)
+        print("self.identity : ", self.identity)
+        print("self.classifier : ", self.classifier)
+        self.model_weights =[]
+        self.conv_layers = []# get all the model children as list
+        self.model_children = list(self.features.children())#counter to keep count of the conv layers
+        counter = 0#append all the conv layers and their respective wights to the list
+        for i in range(len(self.model_children)):
+            if type(self.model_children[i]) == nn.Conv2d:
+                counter+=1
+                self.model_weights.append(self.model_children[i].weight)
+                self.conv_layers.append(self.model_children[i])
+                last_conv_layer = self.model_children[i]
+            elif type(self.model_children[i]) == nn.Sequential:
+                for j in range(len(self.model_children[i])):
+                    for child in self.model_children[i][j].children():
+                        if type(child) == nn.Conv2d:
+                            counter+=1
+                            self.model_weights.append(child.weight)
+                            self.conv_layers.append(child)
+                            last_conv_layer = child
+        print(f"Total convolution layers: {counter}")
+        print("conv_layers")
+        self.last_conv_layer = last_conv_layer
+        
+        
+    def _calculate_num_features(self, replace_from_layer):
+        # Create a temporary tensor to get the shape
+        x = torch.randn(32, 3, 128, 128).to(self.device)  # Batch size of 1, 3 channels, 224x224 image
+        x = self.features(x)
+        print("\nCalculate num features : ", x.shape)
+        num_features = x.view(x.size(0), -1).shape[1]
+        # num_features = x.shape[-1] #  * x.shape[2] * x.shape[3]
+        print("num_features : ", num_features)
+        return num_features
+
+    def forward(self, x):
+        # print("x shape : ", x.shape)
+        x = self.features(x)
+        x = self.identity(x)
+        x = x.view(x.size(0), -1)  # Flatten the feature map
+        x = self.classifier(x)
+        return x
+    
+    def save_checkpoints(self, inputs_ckpt=None):
+        if inputs_ckpt:
+            self.inputs_ckpt = inputs_ckpt
+        torch.save(self.state_dict(), self.inputs_ckpt)
+
+    def load_checkpoint(self, inputs_ckpt=None):
+        if inputs_ckpt:
+            self.inputs_ckpt = inputs_ckpt
+        states = torch.load(self.inputs_ckpt, map_location=self.device)
+        self.load_state_dict(states)
+
+
+
 class ResNet(nn.Module):
     def __init__(self, device=None, path = "checkpoints/resnet.pth", nbr_classes=2, number_conv_layers=2):
         super(ResNet, self).__init__()
         self.model = torchvision.models.resnet50(pretrained=True)
         num_ftrs = self.model.fc.in_features
         n_classes = nbr_classes
-        self.model.fc = nn.Linear(num_ftrs, n_classes)
+        # self.model.fc = nn.Linear(num_ftrs, n_classes)
         
         if not device:
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.device = device
         self.inputs_ckpt = os.path.join("checkpoints", "resnet.pth")
         
+        
+        for idx, module in enumerate(self.model.children()):
+            if idx >= number_conv_layers:
+                for param in module.parameters():
+                    param.requires_grad = False  # Freeze the layers you want to replace
+                self.model[idx] = Identity()  # Replace the layer with Identity
+
+        self.model.fc = nn.Linear(self.model.fc.in_features, nbr_classes)
+
+        # Step 3: If you want to fine-tune the model, unfreeze the modified layers
+        for idx, module in enumerate(self.model.children()):
+            if idx >= number_conv_layers:
+                for param in module.parameters():
+                    param.requires_grad = True
+
+        """
         # Initialize a new model with the same architecture as self.model
         new_model = []
         conv_layer_count = 0
@@ -87,8 +184,9 @@ class ResNet(nn.Module):
         print("Original Model : ", self.model)
         print("\n\n\n\nNew model : ", new_model)
         self.model = new_model
+        """
         self.model.to(self.device)
-        
+        print("Model loaded to device : ", self.model)
         # we will save the conv layer weights in this list
         self.model_weights =[]
         #we will save the 49 conv layers in this list
